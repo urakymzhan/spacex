7ebe6f236ddba2881c0dc1fe766778478eb1e070
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-07-06 23:37:21 -0700

    fixed server issue

---
+++ public/build/precache-manifest.a92f800fa20cc3d14f81278a6954b67a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a11c0da1d25f4792a6063ecaeb1856f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2192c018df941cb59760",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "70f7225b3b9f08385f05",
+    "url": "/static/js/2.74191eff.chunk.js"
+  },
+  {
+    "revision": "d238e78a2900068cc959425de1c25d1e",
+    "url": "/static/js/2.74191eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2192c018df941cb59760",
+    "url": "/static/js/main.674a2ddf.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
